b32a266852976aa8a7a7d65bd92e755945b5f22b
Vinayagavelkumar <Vinayagavelkumar@github.com>
2023-12-31 18:20:35 +0530

    Env changes

--- UI/LuminaUI/src/environments/environment.ts
+++ UI/LuminaUI/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  pmsAPI: 'https://localhost:7117/api/PMS/',
-  prmsAPI: 'https://localhost:7117/api/PRMS/',
-  slmsAPI: 'https://localhost:7117/api/SLMS/',
-  umsAPI: 'https://localhost:7117/api/UMS/'
+  pmsAPI: 'https://localhost/api/PMS/',
+  prmsAPI: 'https://localhost/api/PRMS/',
+  slmsAPI: 'https://localhost/api/SLMS/',
+  umsAPI: 'https://localhost/api/UMS/'
 };
